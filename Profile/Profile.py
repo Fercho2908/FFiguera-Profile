7e4178d14345f2a115e26f8de171aeb815f9d109
Fercho <fefc2908@gmail.com>
2024-08-17 20:24:49 -0400

    Responsive Design

--- Profile/Profile.py
+++ Profile/Profile.py
@@ -1,122 +1,63 @@
 import reflex as rx
 from rxconfig import config
+from Profile.comps import *
 
 def index() -> rx.Component:
     return rx.container(
-        rx.hstack(
+        rx.flex(
             rx.vstack(
                 rx.center(
-                    rx.avatar(
-                        src="profile1.webp",
-                        radius="full",
-                        size="9",
-                    ),
-                    rx.heading("Fernando Figuera", margin_top="0.4em"),
-                    width="100%",
-                    direction="column",
+                    profile_image(),
+                    width="100%"
                 ),
-                rx.center(
-                    rx.text("Habilidades:", size="1"),
-                    rx.flex(
-                        rx.badge("Python", size="2", variant="outline"),
-                        rx.badge("Java", size="2", variant="outline"),
-                        rx.badge("C#", size="2", variant="outline"),
-                        rx.badge("Visual Basic", size="2", variant="outline"),
-                        rx.badge("Git", size="2", variant="outline"),
-                        rx.badge("MySQL", size="2", variant="outline"),
-                        rx.badge("Access", size="2", variant="outline"),
-                        width="80%",
-                        spacing="2",
-                        flex_wrap="wrap",
-                        align="center",
-                        justify="center",
-                        padding_top="0.2em",
-                    ),
-                    width="100%",
-                    direction="column",
-                    padding_top="2.2em",
-                ),
-                
+                skills(),                
                 
                 bg="rgb(56,68,77)",
                 min_width="20em",
-                min_height="65vh",
-                border_radius="1.5rem 0 0 1.5rem",
+                height="auto",
+                padding="0.8em",
+                border_radius=rx.breakpoints(
+                    initial="1.5rem 1.5rem 0 0",
+                    sm="1.5rem 0 0 1.5rem"
+                ),
                 justify="center",
             ),
             rx.vstack(
-                rx.markdown(
-                  """
-¡Hola👋🏽, bienvenido a mi página web!
-
-Soy Fernando Figuera, Técnico Superior Universitario en Informática y actualmente estudiante de Ingeniería en Sistemas en el [IUP Santiago Mariño](http://psmmaturin.edu.ve/).
-
-Soy un apasionado de la tecnología y la programación en constante crecimiento. Esta página es mi proyecto inicial en [Reflex](https://reflex.dev) desarrollado completamente desde cero a partir de su documentación.
-
-Si te interesan mis servicios, no dudes en contactarme. 🚀
-
-                  """  
-                ),
+                profile_info(),
                 rx.flex(
                     rx.hstack(
-                        rx.link(
-                            rx.button(
-                                rx.icon(tag="mail"),
-                                "Contactar",
-                                size="3",
-                                variant="solid",
-                                cursor="pointer"
-                            ),
-                            href="mailto:fefc2908@gmail.com",
-                        ),
-                        rx.button(
-                            rx.icon(tag="file-down"),
-                            "Descargar CV",
-                            on_click=rx.download(url="/CV Fernando.zip"),
-                            size="3",
-                            variant="surface",
-                            cursor="pointer",
-                        ),
+                        contact_button(),
+                        download_cv(),
                         spacing="2"
                     ),
-                    rx.hstack(
-                        rx.link(
-                            rx.icon(tag="Github"),
-                            href="https://github.com/fercho2908",
-                            size="9",
-                            alt="GitHub",
-                            cursor="pointer",
-                        ),
-                        rx.link(
-                            rx.icon(tag="phone"),
-                            href="https://wa.me/584249246863",
-                            size="9",
-                            alt="Whatsapp",
-                            cursor="pointer",
-                        ),
-                        rx.link(
-                            rx.icon(tag="Instagram"),
-                            href="https://instagram.com/fercho.2908",
-                            size="9",
-                            alt="Instagram",
-                            cursor="pointer",
-                        ),
-                        spacing="2",
-                        padding_right="1em"
-                    ),
+                    social_links(),
                     width="100%",
-                    align="end",
-                    justify="between",
+                    align=rx.breakpoints(
+                        initial="center",
+                        xs="end"
+                    ),
+                    justify=rx.breakpoints(
+                        initial="center",
+                        xs="between"
+                    ),
+                    direction=rx.breakpoints(
+                        initial="column",
+                        xs="row"
+                    )
                 ),
                 padding="0.8em",
             ),
             
             bg="rgb(29,42,53)",
-            min_height="65vh",
+            height="100%",
             min_width="70vw",
-            margin_top="13vh",
+            margin_top=rx.breakpoints(initial="2vh", xs="7vh", sm="13vh"),
             margin_bottom="9vh",
             border_radius="1.5rem",
+            direction=rx.breakpoints(
+                initial="column",
+                sm="row"
+            )
         ),
         rx.logo(),
     )
---
+++ Profile/comps.py
@@ -0,0 +1,100 @@
+import reflex as rx
+
+def profile_image():
+    return rx.center(
+        rx.avatar(
+            src="profile1.webp",
+            radius="full",
+            size="9",
+        ),
+        rx.heading("Fernando Figuera", margin_top="0.4em"),
+        direction="column",
+    )
+    
+def skills():
+    return rx.center(
+        rx.text("Habilidades:", size="1"),
+        rx.flex(
+            rx.badge("Python", size="2", variant="outline"),
+            rx.badge("Java", size="2", variant="outline"),
+            rx.badge("C#", size="2", variant="outline"),
+            rx.badge("Visual Basic", size="2", variant="outline"),
+            rx.badge("Git", size="2", variant="outline"),
+            rx.badge("MySQL", size="2", variant="outline"),
+            rx.badge("Access", size="2", variant="outline"),
+            width="80%",
+            spacing="2",
+            flex_wrap="wrap",
+            align="center",
+            justify="center",
+            padding_top="0.2em",
+        ),
+        width="100%",
+        direction="column",
+        padding_top=rx.breakpoints(
+            initial="1.4em",
+            sm="2.2em"
+        ),
+    )
+    
+def profile_info():
+    return rx.markdown(
+        """
+¡Hola👋🏽, bienvenido a mi página web!
+
+Soy Fernando Figuera, Técnico Superior Universitario en Informática y actualmente estudiante de Ingeniería en Sistemas en el [IUP Santiago Mariño](http://psmmaturin.edu.ve/).
+
+Soy un apasionado de la tecnología y la programación en constante crecimiento. Esta página es mi proyecto inicial en [Reflex](https://reflex.dev) desarrollado completamente desde cero a partir de su documentación.
+
+Si te interesan mis servicios, no dudes en contactarme. 🚀
+
+        """  
+    )
+    
+def contact_button():
+    return rx.link(
+        rx.button(
+            rx.icon(tag="mail"),
+            "Contactar",
+            size=rx.breakpoints(
+                initial="2",
+                xs="3"
+            ),
+            variant="solid",
+            cursor="pointer"
+        ),
+        href="mailto:fefc2908@gmail.com",
+    )
+    
+def download_cv():
+    return rx.button(
+        rx.icon(tag="file-down"),
+        "Descargar CV",
+        on_click=rx.download(url="/CV Fernando.zip"),
+        size=rx.breakpoints(
+                initial="2",
+                xs="3"
+            ),
+        variant="surface",
+        cursor="pointer",
+    )
+    
+def social_icon(icon, link):
+    return rx.link(
+        rx.icon(icon),
+        href=link,
+    )
+    
+def social_links():
+    return rx.hstack(
+        social_icon("github", "https://github.com/Fefc2908"),
+        social_icon("Phone", "https://wa.me/584249246863"),
+        social_icon("Instagram", "https://instagram.com/fercho.2908"),
+        spacing="2",
+        padding=rx.breakpoints(
+            initial="0.8em 0 0 0",
+            xs="0.8 0.4em 0 0",
+            sm="0 1em 0 0"
+        ),
+        padding_right="1em"
+    )
